f017fff1e61b5e0a81f6b8b2639bbd7e91519e10
g00341965 <35809401+g00341965@users.noreply.github.com>
2018-01-31 14:17:55 +0000

    Add files via upload

--- add-content.js
+++ add-content.js
@@ -2,22 +2,26 @@ var today = new Date()
 var hourNow = today.getHours();
 var greeting;
 
-var day = today.getDay();
-var weekDay;
+var weekDay = today.getDay();
 
-if (day==3){
-	weekDay="Wednesday";
-}
+
+var daysOfTheWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+console.log("the day of the week " + weekDay);
+
+console.log("the days of the week " + daysOfTheWeek);
+
 
 if(hourNow>=18) {
-	greeting='Good evening, its ' + weekDay;
+	greeting='Good evening, its ' + daysOfTheWeek[weekDay];
 } else if (hourNow>12) {
-	greeting='Good afternoon Pauline! Happy ' + weekDay;
+	greeting='Good afternoon Pauline! Happy ' + daysOfTheWeek[weekDay];
 } else if (hourNow>0) {
-	greeting='Good morning Pauline! Happy ' + WeekDay;
+	greeting='Good morning Pauline! Happy ' + daysOfTheWeek[weekDay];
 } else {
 	greeting='Welcome!';
 }
+
 
 
 
